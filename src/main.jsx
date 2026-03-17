5dbcc05125739885a113142b7633688254980c4b
bigticket-logistica <camilonaranjo.bigticket@gmail.com>
2026-03-17 19:01:06 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../logiflow-dashboard-v3'
+import App from '../logiflow-dashboard-v4'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
